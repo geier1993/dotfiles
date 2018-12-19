43a78abf3d00518aec0a291604db107dc059f4dd
Philipp Geier <Philippgeier93@hotmail.de>
2018-12-19 07:11:50 +0100

    Samba Krauth added

--- configuration_krauth.nix
+++ configuration_krauth.nix
@@ -63,6 +63,26 @@
 
   environment.shells = with pkgs; [bashInteractive zsh];
 
+
+  fileSystems."/mnt/KT-Dat" = {
+      device = "//ebfile01/KT-Dat";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+  fileSystems."/mnt/ebfile01" = {
+      device = "//ebfile01";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
