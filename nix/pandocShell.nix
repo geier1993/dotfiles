99c223d12d7a8488a066b47aefcc6673291747b4
Philipp Geier <Philippgeier93@hotmail.de>
2019-04-25 21:44:58 +0200

    changed i3 color theme and added pandoc env

--- nix/pandocShell.nix
+++ nix/pandocShell.nix
@@ -1,11 +1,13 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation {
-  name = "my-environment";
+  name = "pandocShell";
 
   buildInputs = with pkgs; [
-    texlive.combined.scheme-medium
-    #texlive.combined.scheme-full
+    #texlive.combined.scheme-medium
+    # texlive full required because of varwidth.sty
+    texlive.combined.scheme-full
+    python36Packages.python
     pandoc
     haskellPackages.pandoc-citeproc
     haskellPackages.pandoc-crossref
