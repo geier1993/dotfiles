e8501f76e539d95721f860868ae64058292002d5
Philipp Geier <Philippgeier93@hotmail.de>
2019-05-25 11:26:08 +0200

    moving to sdd

--- usb/usb/syncUSB.sh
+++ usb/usb/syncUSB.sh
@@ -10,9 +10,6 @@ gpg --export-secret-keys Philipp@geier.me> /media/usb/philippgeier-secret-gpg.as
 rsync -avrP  --exclude-from 'exclude_sync.txt' /home/geier/Documents /media/usb/ 
 rsync -avrP  --exclude-from 'exclude_sync.txt' /home/geier/Pictures /media/usb/
 
-#rsync -avrP --delete --exclude-from 'exclude_sync.txt' /home/geier/packages.yaml /media/usb/
-rsync -avrP --exclude-from 'exclude_sync.txt' /home/geier/todo.yaml /media/usb/
-
 rsync -avrP --exclude-from 'exclude_sync.txt' /home/geier/.config /media/usb/
 
 rsync -avrP --exclude-from 'exclude_sync.txt' /home/geier/misc/Soilphysics /media/usb/misc/
