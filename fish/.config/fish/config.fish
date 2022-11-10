f6dff5c9e20690e03deb79688eca48f02136e156
Philipp Geier <philipp.geier@ecmwf.int>
2022-11-10 10:58:32 +0100

    Fix rg-fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -160,7 +160,7 @@ function rga-fzf
 end
 
 function rg-fzf
-    set RG_PREFIX 'rga --files-with-matches'
+    set RG_PREFIX 'rg --files-with-matches'
     if test (count $argv) -gt 1
         set RG_PREFIX "$RG_PREFIX $argv[1..-2]"
     end
@@ -177,7 +177,7 @@ function rg-fzf
         FZF_DEFAULT_COMMAND="$RG_PREFIX '$argv[-1]'" \
         fzf --sort \
             --preview='test ! -z {} && \
-                rga --pretty --context 5 {q} {}' \
+                rg --pretty --context 5 {q} {}' \
             --phony -q "$argv[-1]" \
             --bind "change:reload:$RG_PREFIX {q}" \
             --preview-window='50%:wrap'
