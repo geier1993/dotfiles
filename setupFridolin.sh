26c7a0dce14ab08cd1a3173ac7dcceb46634f892
Philipp Geier <philippgeier93@hotmail.de>
2022-12-31 21:19:07 +0100

    Add fridolin...

---
+++ setupFridolin.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+TARGET=~/
+
+stow -t $TARGET bin
+stow -t $TARGET fish
+stow -t $TARGET direnv
+stow -t $TARGET zellij-n1
+stow -t $TARGET ranger
+stow -t $TARGET readline
+stow -t $TARGET kak
+stow -t $TARGET fzf
+stow -t $TARGET usb
